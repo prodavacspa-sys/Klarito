86bbd484c4ee9b9c28a99195ebcbf48fcac01285
Josip Sekul <prodavac.spa@gmail.com>
2026-05-13 23:08:49 -0400

    fix: usar dominio resend.dev temporalmente hasta verificar klarito.cl

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,7 +1,7 @@
 import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
-const FROM = 'Klarito <hola@klarito.cl>'
+const FROM = 'Klarito <onboarding@resend.dev>'
 
 export async function sendWelcomeEmail(email: string, businessName: string) {
   await resend.emails.send({
